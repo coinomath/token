32cfb67e70a8766e1c78a15be65e4a5c3b487935
Jackie <mengjq08@gmail.com>
2017-06-20 14:01:20 +0800

    add finish control function

--- contracts/BNToken.sol
+++ contracts/BNToken.sol
@@ -16,23 +16,25 @@ contract BNToken is StandardToken, SafeMath {
 
     bool public isFinalized;    // if the crowdsale is finished.
     uint256 public etherCap = 100000 * 10**decimals;    // the most ether for crowdsale
-    uint256 public tokenCap = 2 * 10**8;
+    uint256 public etherMin = 50000 * 10**decimals;     // the minest ether for crowdsale
+    uint256 public tokenCap = 2 * (10**8) * 10**decimals;
+    uint256 public totalInvestment;
+    mapping (address => uint256) investments;
     
+    event Invest(address indexed investor, uint256 value);
     event Allocation(address indexed investor, uint256 value);
+    event Refund(address indexed investor, uint256 value);
 
     function BNToken(
         address _founder,
-        uint256 _startBlock,
-        uint256 _endBlock
+        uint256 _startBlock
     ) {
-        if (_endBlock <= _startBlock) throw;
-
         founder = _founder;
         startBlock = _startBlock;
-        endBlock = _endBlock;
+        endBlock = startBlock + 4 * 7 * 60000; // 4 weeks
 
         isFinalized = false;    // default to false
-        totalSupply = 15 * 10**6;
+        totalSupply = 15 * (10**6) * 10**decimals;
         balances[founder] = totalSupply;
         Allocation(founder, totalSupply);
     }
@@ -49,6 +51,10 @@ contract BNToken is StandardToken, SafeMath {
         uint256 newSupply = safeAdd(totalSupply, tokens);
         if (tokenCap < newSupply) throw;
         
+        investments[msg.sender] = msg.value;
+        totalInvestment = safeAdd(totalInvestment, msg.value);
+        Invest(msg.sender, msg.value);
+
         totalSupply = newSupply;
         balances[msg.sender] += tokens;
         Allocation(msg.sender, tokens);
@@ -59,12 +65,46 @@ contract BNToken is StandardToken, SafeMath {
      * get rate on realtime
      */
     function rate() constant returns(uint256) {
-        if (block.number < startBlock + 250) return 3300;    // early bird
-        if (block.number < startBlock + 420000) return 3000;
-        if (block.number < startBlock + 840000) return 2750;
-        if (block.number < startBlock + 1260000) return 2500;
-        if (block.number <= endBlock) return 2250; 
+        if (block.number < startBlock + 250) return 3300;       // early bird
+        if (block.number < startBlock + 420000) return 3000;    // price of first week
+        if (block.number < startBlock + 840000) return 2750;    // price of second week
+        if (block.number < startBlock + 1260000) return 2500;   // price of third week
+        if (block.number <= endBlock) return 2250;              // price of last(fourth) week
         throw;
     }
 
+    /**
+     * finish the crowdsale
+     */
+    function finalized() external {
+        if (isFinalized) throw;
+        if (msg.sender != founder) throw;
+        if (totalInvestment < etherMin) throw;
+        if (block.number <= endBlock) throw;
+        isFinalized = true;
+        if (!founder.send(this.balance)) throw;
+    }
+
+    /**
+     * refund
+     * use for refunding on crowdsale faild
+     */
+    function refund() external {
+        if (isFinalized) throw;
+        if (block.number <= endBlock) throw;
+        if (totalInvestment >= etherMin) throw;
+        if (msg.sender == founder) throw;
+
+        uint value = invests[msg.sender];
+        if (value <= 0) throw;
+        totalInvestment = safeSubtract(totalInvestment, value);
+        totalSupply = safeSubtract(totalSupply, balances[msg.sender]);
+        investments[msg.sender] = 0;
+        balances[msg.sender] = 0;
+
+        // send assets
+        // notice that if you're using a contract; make sure it works with .send gas limits
+        if (!msg.sender.send(value)) throw;
+        Refund(msg.sender, value); 
+    }
 }
